a63cd3beba6b4fcee286c8b6d1c615ec2afb7308
jamietech <jamie@jamiete.ch>
2017-05-01 00:42:44 +1000

    Format music logs and log to disk

--- src/ch/jamiete/hilda/music/MusicPlugin.java
+++ src/ch/jamiete/hilda/music/MusicPlugin.java
@@ -1,6 +1,14 @@
 package ch.jamiete.hilda.music;
 
+import java.io.File;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.logging.ConsoleHandler;
+import java.util.logging.FileHandler;
+import java.util.logging.Level;
 import ch.jamiete.hilda.Hilda;
+import ch.jamiete.hilda.LogFormat;
+import ch.jamiete.hilda.Start;
 import ch.jamiete.hilda.music.commands.MusicBaseCommand;
 import ch.jamiete.hilda.plugins.HildaPlugin;
 
@@ -16,6 +24,30 @@ public class MusicPlugin extends HildaPlugin {
     public void onEnable() {
         this.music = new MusicManager(this.getHilda(), this);
         this.getHilda().getCommandManager().registerChannelCommand(new MusicBaseCommand(this.getHilda(), this.music));
+
+        MusicManager.getLogger().setUseParentHandlers(false);
+        final ConsoleHandler handler = new ConsoleHandler();
+        handler.setFormatter(new LogFormat());
+        MusicManager.getLogger().addHandler(handler);
+
+        try {
+            final File file = new File("log");
+
+            if (!file.isDirectory()) {
+                file.mkdir();
+            }
+
+            final FileHandler lfh = new FileHandler("log/hilda_music_" + new SimpleDateFormat("dd-MM-yyyy").format(Calendar.getInstance().getTime()) + ".log", true);
+            lfh.setFormatter(new LogFormat());
+            MusicManager.getLogger().addHandler(lfh);
+        } catch (final Exception e) {
+            e.printStackTrace();
+        }
+
+        if (Start.DEBUG) {
+            handler.setLevel(Level.FINE);
+            MusicManager.getLogger().setLevel(Level.FINE);
+        }
     }
 
     public MusicManager getMusicManager() {
