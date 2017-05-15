7c26254a2f7a8f2c1e1f0d39e469361ca1e12cd7
jamietech <jamie@jamiete.ch>
2017-05-15 16:32:28 +1000

    Rotate log files at midnight

--- src/ch/jamiete/hilda/music/MusicPlugin.java
+++ src/ch/jamiete/hilda/music/MusicPlugin.java
@@ -3,13 +3,17 @@ package ch.jamiete.hilda.music;
 import java.io.File;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
+import java.util.concurrent.TimeUnit;
 import java.util.logging.ConsoleHandler;
 import java.util.logging.FileHandler;
+import java.util.logging.Handler;
 import java.util.logging.Level;
 import ch.jamiete.hilda.Hilda;
 import ch.jamiete.hilda.LogFormat;
 import ch.jamiete.hilda.Start;
+import ch.jamiete.hilda.Util;
 import ch.jamiete.hilda.music.commands.MusicBaseCommand;
+import ch.jamiete.hilda.music.tasks.LogRotateTask;
 import ch.jamiete.hilda.plugins.HildaPlugin;
 
 public class MusicPlugin extends HildaPlugin {
@@ -24,8 +28,22 @@ public class MusicPlugin extends HildaPlugin {
     public void onEnable() {
         this.music = new MusicManager(this.getHilda(), this);
         this.getHilda().getCommandManager().registerChannelCommand(new MusicBaseCommand(this.getHilda(), this.music));
+        this.setupLogging();
 
+        final long rotate = this.getHilda().getNextMidnightInMillis("GMT+10") - System.currentTimeMillis();
+        this.getHilda().getExecutor().scheduleAtFixedRate(new LogRotateTask(this), rotate, 86400000, TimeUnit.MILLISECONDS); // At midnight then every 24 hours
+        Hilda.getLogger().info("Rotating log files in " + Util.getFriendlyTime(rotate));
+    }
+
+    public void setupLogging() {
         MusicManager.getLogger().setUseParentHandlers(false);
+
+        for (Handler handler : MusicManager.getLogger().getHandlers()) {
+            if (handler instanceof ConsoleHandler) {
+                MusicManager.getLogger().removeHandler(handler);
+            }
+        }
+
         final ConsoleHandler handler = new ConsoleHandler();
         handler.setFormatter(new LogFormat());
         MusicManager.getLogger().addHandler(handler);
